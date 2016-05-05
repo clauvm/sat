f6b5c627e257901112fba21f3986c9d045b61244
clauvm <claudiavaqueram@gmail.com>
2016-05-05 11:42:06 -0400

    cambio en index

--- js/controllers.js
+++ js/controllers.js
@@ -1,6 +1,57 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function($scope) {
+
+     var client = new Messaging.Client("test.mosquitto.org", 8080, "myclientid_" + parseInt(Math.random() * 100, 10));
+ console.log("en lista");
+//Connect Options
+ var options = {
+     timeout: 3,
+     //Gets Called if the connection has sucessfully been established
+     onSuccess: function () {
+         //alert("Connected");
+          //$('#estado').text('Conectado');
+         client.subscribe('/prueba/server', {qos: 2});
+         alert("conectado mqtt!");
+     },
+     //Gets Called if the connection could not be established
+     onFailure: function (message) {
+        //$('#estado').text('Error de conexion:');
+        console.log('Error de conexion');
+        //conectar();
+        // alert("Connection failed: " + message.errorMessage);
+     }
+ };
+ $scope.connect = function () {
+    client.connect(options);
+ }
+ 
+ $scope.publicar=function () {
+    
+ };
+// function conectar () {
+//      client.connect(options);
+// }
+// function erase(){
+//     $('#messages').empty();
+
+// }
+
+ //Gets  called if the websocket/mqtt connection gets disconnected for any reason
+ // client.onConnectionLost = function (responseObject) {
+ //     //Depending on your scenario you could implement a reconnect logic here
+ //      $('#messages').empty();
+ //      $('#estado').text('Desconectado');
+ //     //alert("connection lost: " + responseObject.errorMessage)
+
+ // };
+
+ //Gets called whenever you receive a message for your subscriptions
+ client.onMessageArrived = function (message) {
+     //Do something with the push message you received
+     $('#messages').append('<span>' + message.payloadString + '</span><br/>');
+ }; 
+})
 
 .controller('UsersCtrl', function($scope, Users, $stateParams) {
   // With the new view caching in Ionic, Controllers are only called
